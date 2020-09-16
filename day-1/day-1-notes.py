29a07ac09bf681b76ab2832554d19fb9a1179255
Tyler Alsop <tylerjalsop@gmail.com>
2020-09-15 20:24:06 -0600

    Added notes from the behavioral interview portion of the standup.

---
+++ day-1/day-1-notes.py
@@ -0,0 +1,40 @@
+Tell me a little bit about yourself:
+
+    "They need to answer this question with their Elevator pitch: (past - their background, present - how they found Lambda what they’re learning at Lambda a good time to humblebrag about technical skills, future - tie in background and new skills to talk about potential future job)"
+
+    I repeated myself too many times and had a few pauses that were longer than I would have liked.
+
+    - 30 sec to 1 min Elevator pitch
+
+    - Talk about
+        - Personal life breifly
+        - How I found Lambda School
+        - Maybe bring up the why
+        - Humble brag about technical skills
+        - 
+
+
+What kind of work environment are you looking for in the next opportunity you take?:
+
+    "Be specific, know about what you want in terms of company culture, size, mentor, etc…try to rope in some specifics about the company (you should have some background on them)"
+
+    - Be specific about what you want
+    - Bring up things that relate to the company
+
+
+
+What are your strengths and weaknesses as a developer?:
+
+    "First way to answer this question is to name a weakness that’s not really a weakness i.e. - “I’m too much of a perfectionist” etc... Second way is to actually provide a weakness but then without being prompted provide how you’re going to FIX the weakness"
+
+
+
+
+
+Steve: Spoke really fast
+
+Sheila: Lighting could have been better
+
+Bradley: Lost his groove, could smile more. Nailed it the second time.
+
+Jose: Did well. I started taking notes too late and don't remember too much. I do remember the previous jobs though! Currently a lawer and previously a professional poker player. Mentioning them really made him stand out.
